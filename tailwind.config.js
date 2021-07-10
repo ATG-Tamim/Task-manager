7c19f7d3a44e38574c5149897802d59ce0f2b629
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-07-10 09:35:31 +0430

    Customize tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        13: '1000rem !important',
+      },
+      keyframes: {
+        wiggle: {
+          '0%,100%': {
+            backgroundColor: '#9DA1A8',
+            opacity: 0.7,
+          },
+          '50%': {
+            background: '#d1d5db',
+          },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+    variants: {
+      margin: ['last', 'first', 'responsive'],
+    },
+    plugins: [],
   },
-  variants: {
-    margin: ['last', 'first', 'responsive'],
-  },
-  plugins: [],
 }
